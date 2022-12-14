b2f9da663411c8167104c3c227c800a21476dcbc
Kevin Bost <kitokeboo@gmail.com>
2022-12-13 23:59:56 -0800

    Adding MVVM example

---
+++ PointerStar/Client/ViewModels/CounterViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PointerStar.Client.ViewModels;
+
+public partial class CounterViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private int _currentCount;
+    
+    public void IncrementCount()
+    {
+        CurrentCount++;
+    }
+}
---
+++ PointerStar/Client/ViewModels/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿namespace PointerStar.Client.ViewModels;
+
+public abstract partial class ViewModelBase : ObservableObject
+{
+    public virtual async Task OnInitializedAsync()
+        => await Loaded().ConfigureAwait(true);
+
+    protected virtual void NotifyStateChanged() => OnPropertyChanged("");
+
+    [RelayCommand]
+    public virtual Task Loaded()
+        => Task.CompletedTask;
+}
