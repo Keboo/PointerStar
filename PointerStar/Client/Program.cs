a8203d6804778d61a38d46d8e02b02b737dce505
Kevin Bost <kitokeboo@gmail.com>
2022-12-14 22:16:42 -0800

    Passing in room id and name

--- PointerStar/Client/Program.cs
+++ PointerStar/Client/Program.cs
@@ -10,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<CounterViewModel>();
+builder.Services.AddScoped<RoomViewModel>();
 
 await builder.Build().RunAsync();
