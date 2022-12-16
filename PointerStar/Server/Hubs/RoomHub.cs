b208dff477fc79fad303227bad4bd96813b1d19f
Kevin Bost <kitokeboo@gmail.com>
2022-12-16 00:10:43 -0800

    Starting adding SignalR Hub

---
+++ PointerStar/Server/Hubs/RoomHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace PointerStar.Server.Hubs;
+
+public class RoomHub : Hub
+{
+    public override Task OnDisconnectedAsync(Exception? exception) => base.OnDisconnectedAsync(exception);
+
+    public override Task OnConnectedAsync() => base.OnConnectedAsync();
+
+    [HubMethodName(nameof(JoinRoom))]
+    public async Task JoinRoom(string roomId)
+        => await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+}
