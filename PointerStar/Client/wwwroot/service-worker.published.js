69c2a35223c2fac9226aa5ba625d74e45865115f
Kevin Bost <kitokeboo@gmail.com>
2023-01-25 10:26:37 -0800

    Testing out PWAUpdater package
    https://github.com/jsakamoto/Toolbelt.Blazor.PWA.Updater

--- PointerStar/Client/wwwroot/service-worker.published.js
+++ PointerStar/Client/wwwroot/service-worker.published.js
@@ -8,7 +8,7 @@ self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
-const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
+const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
 const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 
 async function onInstall(event) {
@@ -47,3 +47,8 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
+
+// https://github.com/jsakamoto/Toolbelt.Blazor.PWA.Updater 
+self.addEventListener('message', event => {
+    if (event.data?.type === 'SKIP_WAITING') self.skipWaiting();
+});
