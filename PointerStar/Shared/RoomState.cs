cc8d207a07a5c315bc04efb02143a6e7d553b3fa
Allen Monroe <allen.monroe@gmail.com>
2023-02-10 10:37:36 -0800

    Issue 106 - set default room view to autoshowvotes = true

--- PointerStar/Shared/RoomState.cs
+++ PointerStar/Shared/RoomState.cs
@@ -12,7 +12,7 @@ public record class RoomState(string RoomId, User[] Users)
         => Users.Where(u => u.Role == Role.Observer).ToList();
 
     public bool VotesShown { get; init; }
-    public bool AutoShowVotes { get; init; }
+    public bool AutoShowVotes { get; init; } = true;
     
     public string[] VoteOptions { get; init; } = new[]
     {
