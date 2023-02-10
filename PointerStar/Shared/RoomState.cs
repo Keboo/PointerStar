f0a810ef850c46235133e0cddefd5af5113e4ba3
Allen Monroe <allen.monroe@gmail.com>
2023-02-10 10:50:29 -0800

    Updating unit tests and changing default AuthShowVotes to true

--- PointerStar/Shared/RoomState.cs
+++ PointerStar/Shared/RoomState.cs
@@ -12,6 +12,7 @@ public record class RoomState(string RoomId, User[] Users)
         => Users.Where(u => u.Role == Role.Observer).ToList();
 
     public bool VotesShown { get; init; }
+    //We want the default to be true so new facilitators have this on by default
     public bool AutoShowVotes { get; init; } = true;
     
     public string[] VoteOptions { get; init; } = new[]
