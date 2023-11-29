a40827e7efc7ab5e570ce0ac4441ffd523c81aa0
Kenny White <kennival@hotmail.com>
2023-11-29 10:44:53 -0800

    Update user name max length (#233)
    * Update user name max length
    
    * Update tests

--- PointerStar/Shared/RoomState.cs
+++ PointerStar/Shared/RoomState.cs
@@ -14,7 +14,7 @@ public record class RoomState(string RoomId, User[] Users)
     public bool VotesShown { get; init; }
     //We want the default to be true so new facilitators have this on by default
     public bool AutoShowVotes { get; init; } = true;
-    
+
     public string[] VoteOptions { get; init; } = new[]
     {
         "1",
@@ -35,7 +35,7 @@ public record class RoomState(string RoomId, User[] Users)
 
 public record class User(Guid Id, string Name)
 {
-    public const int MaxNameLength = 20;
+    public const int MaxNameLength = 40;
 
     public string? OriginalVote { get; init; }
     public string? Vote { get; init; }
@@ -47,7 +47,7 @@ public record class Role(Guid Id, string Name)
     private static readonly Guid FacilitatorId = new("5fea7d71-fb62-405c-823c-09752c684bf0");
     private static readonly Guid TeamMemberId = new("116b133b-b16d-4a92-a3ce-ae53688e973c");
     private static readonly Guid ObserverId = new("a0fec1ad-caee-4fa0-8d93-d0ce970f92d7");
-    
+
     public static Role Facilitator { get; } = new(FacilitatorId, "Facilitator");
     public static Role TeamMember { get; } = new(TeamMemberId, "Team Member");
     public static Role Observer { get; } = new(ObserverId, "Observer");
